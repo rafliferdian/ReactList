89c221c78c49740f7e9c2f98afadf4fd3525ef9e
rafliferdian <rafli_ferdian_29RPL@student.smktelkom-mlg.sch.id>
2022-03-24 04:06:18 +0700

    berhasil

--- src/Components/utama.js
+++ src/Components/utama.js
@@ -2,24 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Beranda from "./beranda";
-import Siswa from "./siswa";
-import Kelas from "./kelas";
-import Spp from "./spp";
-import Petugas from "./petugas";
-import Transaksi from "./transaksi";
-import Histori from "./histori";
-import Logout from "./logout";
+import Gallery from "./gallery";
 
 const Utama = () => (
   <Routes>
     <Route exact path="/" element={<Beranda />} />
-    <Route path="/siswa" element={<Siswa />} />
-    <Route path="/kelas" element={<Kelas />} />
-    <Route path="/spp" element={<Spp />} />
-    <Route path="/petugas" element={<Petugas />} />
-    <Route path="/transaksi" element={<Transaksi />} />
-    <Route path="/histori" element={<Histori />} />
-    <Route path="/logout" element={<Logout />} />
+    <Route path="/gallery" element={<Gallery />} />
   </Routes>
 );
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand">Pembayaran SPP</a>
+          <p class="navbar-brand">Modul 3</p>
           <button
             class="navbar-toggler"
             type="button"
@@ -26,44 +26,9 @@ const Navbar = () => {
                   Beranda
                 </Link>
               </li>
-              <li class="nav-item dropdown">
-                <Link
-                  to="/"
-                  className="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  role="button"
-                  aria-expanded="false"
-                >
-                  Data
-                </Link>
-                <ul class="dropdown-menu">
-                  <Link to="/siswa" className="dropdown-item">
-                    Siswa
-                  </Link>
-                  <Link to="/kelas" className="dropdown-item">
-                    Kelas
-                  </Link>
-                  <Link to="/spp" className="dropdown-item">
-                    Spp
-                  </Link>
-                  <Link to="/petugas" className="dropdown-item">
-                    Petugas
-                  </Link>
-                </ul>
-              </li>
               <li class="nav-item">
-                <Link to="/transaksi" className="nav-link">
-                  Transaksi
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link to="/histori" className="nav-link">
-                  Histori
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link to="/logout" className="nav-link">
-                  Logout
+                <Link to="/gallery" className="nav-link">
+                  Gallery
                 </Link>
               </li>
             </ul>
